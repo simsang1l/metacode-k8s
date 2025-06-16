72f772d316b60d57c81fa8fbd97efe648369a200
심상일 <simsang1db@gmail.com>
2025-06-16 22:50:31 +0900

    Update hello_dag.py

--- airflow-dags/hello_dag.py
+++ airflow-dags/hello_dag.py
@@ -4,24 +4,25 @@ from datetime import datetime
 
 with DAG(
     dag_id="test_git_sync_dag",
+    start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False,
-    tags=["git", "test"]
+    tags=["git", "test"],
 ) as dag:
 
     start = BashOperator(
         task_id="start_task",
-        bash_command="echo 'Starting Dag execution ...'"
+        bash_command="echo 'Starting DAG execution...'"
     )
 
     middle = BashOperator(
         task_id="middle_task",
-        bash_command="echo 'Starting Dag execution ...'"
+        bash_command="echo 'Middle of DAG.'"
     )
 
     end = BashOperator(
         task_id="end_task",
-        bash_command="echo 'Starting Dag execution ...'"
+        bash_command="echo 'DAG completed.'"
     )
 
     start >> middle >> end
